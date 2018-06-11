63aaa74efe6c35ace90c6ac91de10e00f810d139
Abdul Qadir <abdulqadir93@outlook.com>
2018-06-12 03:21:15 +0500

    access token from each request

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
-    //
+    protected function getToken(Request $request) {
+        $array = $request->all();
+        if (isset($array['token'])) {
+            return $array['token'];
+        }
+        return null;
+    }
 }
